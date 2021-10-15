e6c5b07f1a377b190ee0f19be726ba35ce95f5f1
TrentLS <trent.slutzky@gmail.com>
2021-10-15 14:42:45 -0400

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,5 @@ export PATH="$PATH:/home/trent/Scripts"
 set -x FLAG_QUEST_SECRET_KEY "\xea\x82\xb1@\xac\xb2\x7f\xebdb\xd2\x15b\xfd\xees\xf9{\xf3<@\x8b7\xad"
 set -x FLAG_QUEST_API_KEY "QYb78NSkYipk9skBcxXqBK"
 
-echo test
+wal --theme sexy-kasugano -e -q
+
